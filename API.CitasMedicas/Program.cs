a8cbdb39d65b61136300b4f2889e90cdc4daa8b8
Crimcat <69399291+Crimtom@users.noreply.github.com>
2026-02-09 17:36:33 -0600

    Creación de la Conexión y las entidades. IMPORTANTE CAMBIAR EL SERVIDOR Y LA CLAVE EN LA CONEXIÓN A LA HORA DE PROBAR EL PROGRAMA

--- API.CitasMedicas/Program.cs
+++ API.CitasMedicas/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using DataAcces.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<CitasMedicasContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("cn"));
+});
 
 builder.Services.AddControllers();
 
